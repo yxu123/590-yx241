44d99ad4281a83cfd9984061216e027bd38f149c
yxu123 <78059898+yxu123@users.noreply.github.com>
2021-11-05 12:31:20 -0400

    Create 01-clean.py

---
+++ HW5.0/WIKI/01-clean.py
@@ -0,0 +1,89 @@
+
+import os
+import requests
+from pywikiapi import wikipedia
+
+topics = ["Food China", "Food Moxico"]
+max_pages = 1000
+
+
+def break_the_page(content):
+    leave_content = content
+    index = 0
+    lines = 0
+
+    broken = []
+    start = 0
+    end = 0
+    try:
+        while True:
+            index = content.find('\n',index+1)
+
+            if index < 0:
+                break
+            lines += 1
+            end = index
+
+            if end - start > 1000:
+                broken.append(content[start:end])
+                start = end
+    except:
+        pass
+
+    return broken
+
+
+def query_wikipages(topic):
+
+    titles = wikipedia.search(topic, max_results=max_pages)
+
+    print(topic + " search titles:" + titles)
+    
+    texts = []
+
+    for title in titles:
+        page = wikipedia.page(title,auto_suggest=False)
+        texts += break_the_page(page.content)
+
+    return texts
+
+labels = []
+texts = []
+for label, book in enumerate(topics):    
+    for text in query_wikipages(topics):
+        texts.append(text)
+        labels.append(label)
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+maxlen = 100
+training_samples = int(len(labels) * 0.8)
+validation_samples = len(labels) - training_samples
+max_words = 10000
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+data = pad_sequences(sequences, maxlen=maxlen)
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples: training_samples + validation_samples]
+y_val = labels[training_samples: training_samples + validation_samples]
+
+print(x_train[0:10])
+print(y_train[0:10])
+
+
+import numpy as np
+
+np.savez("wikipages.npz",x_train=x_train,y_train=y_train,x_val=x_val,y_val=y_val)
+
